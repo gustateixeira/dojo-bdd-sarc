9248cfd6cf21f3539471ff01dbb3606cc3fa3e4e
Velloso Gabriel <velloso.g@edu.pucrs.br>
2026-06-05 15:22:03 -0300

    fix: steps

--- src/test/java/cepes/itacademy/runner/CucumberRunnerTest.java
+++ src/test/java/cepes/itacademy/runner/CucumberRunnerTest.java
@@ -10,7 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features") // Aponta para a pasta src/test/resources/features
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.pucrs.itacademy.steps") // Aponta para o pacote dos steps
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps") // Aponta para o pacote dos steps
 public class CucumberRunnerTest {
     // A classe permanece vazia. Ela serve apenas como âncora para as anotações.
 }
